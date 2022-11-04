544990b525b05ba251cb63290e9441acc914badb
{yanagawa} <{shunsuke.yanagawa@ins-saison.co.jp}>
2022-11-04 11:22:06 +0900

    hoge

--- Z.py
+++ Z.py
@@ -22,7 +22,7 @@ import subprocess
 
 FILE_NAME=input('ファイル名を入力（拡張子.xlsmは除く）')
 SHEET_NAME='Z打鍵'
-BASE_BAT_SIZE=2 #バッチサイズ。一度の打鍵件数
+BASE_BAT_SIZE=10 #バッチサイズ。一度の打鍵件数
 
 #githubから最新版をダウンロードする。強制的に上書き
 try:#ダウンロードする際、競合してUnlink *** try?y/n みたいなメッセージで止まるので、いったん削除して回避。
@@ -54,7 +54,7 @@ while len(calc_row) > 0:
         calc_row=list()
         #数値のはいっていない行のうち、バッチサイズの行だけExcel上の行番号を取得（dfのindex+2）する。
         BAT_SIZE=BASE_BAT_SIZE + random.randint(0, BASE_BAT_SIZE) #並列処理時にデータファイルアクセスのタイミングをずらすために乱数を加算
-        calc_row=list(df[(df['車有P'] == 'E') | (df['車有P'].isna())].index[0:BAT_SIZE]+2)
+        calc_row=list(df[(df['車有P'] == 'EE') | (df['車有P'].isna())].index[0:BAT_SIZE]+2)
         print(calc_row)
 
         #打鍵を始める行に打鍵中と入力
