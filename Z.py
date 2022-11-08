786c1605dfd103a16c52ba46ec4dae933b75764f
yanagawa <{shunsuke.yanagawa@ins-saison.co.jp}>
2022-11-08 16:32:39 +0900

    hoge

--- Z.py
+++ Z.py
@@ -17,12 +17,12 @@ import random
 import subprocess
 
 
-# In[2]:
+# In[ ]:
 
 
 FILE_NAME=input('ファイル名を入力（拡張子.xlsmは除く）=')
 SHEET_NAME='Z打鍵'
-BASE_BAT_SIZE=int(input('バッチあたりの打鍵数(>=1)=')) #バッチサイズ。一度の打鍵件数
+BASE_BAT_SIZE=int(input('バッチあたりの打鍵数(>=1)')) #バッチサイズ。一度の打鍵件数
 
 #githubから最新版をダウンロードする。強制的に上書き
 try:#ダウンロードする際、競合してUnlink *** try?y/n みたいなメッセージで止まるので、いったん削除して回避。
@@ -41,8 +41,8 @@ df['車両AMTエラー']=''
 df['新車保険金額エラー']=''
 
 ws=wb[SHEET_NAME]
-ws.cell(row=1, column=78).value = '車両AMTエラー'
-ws.cell(row=1, column=79).value = '新車保険金額エラー'
+ws.cell(row=1, column=df.columns.get_loc('車両AMTエラー')+1).value = '車両AMTエラー'
+ws.cell(row=1, column=df.columns.get_loc('新車保険金額エラー')+1).value = '新車保険金額エラー'
 
 #打鍵する行のリストの箱を用意
 calc_row=list(range(1))
@@ -53,7 +53,7 @@ while len(calc_row) > 0:
     calc_row=list()
     #数値のはいっていない行のうち、バッチサイズの行だけExcel上の行番号を取得（dfのindex+2）する。
     BAT_SIZE=BASE_BAT_SIZE + random.randint(0, BASE_BAT_SIZE) #並列処理時にデータファイルアクセスのタイミングをずらすために乱数を加算
-    calc_row=list(df[df['車有P'].isna()].index[0:BAT_SIZE]+2)
+    calc_row=list(df[(df['車有P'] == 'E') | (df['車有P'].isna())].index[0:BAT_SIZE]+2)
     print(calc_row)
 
     #エラー回数カウントを初期化
@@ -61,8 +61,8 @@ while len(calc_row) > 0:
 
     #打鍵を始める行に打鍵中と入力
     for j in calc_row:
-        ws.cell(row=j, column=73).value = '打鍵中' 
-        df.iloc[j-2,73-1]='打鍵中'
+        ws.cell(row=j, column=df.columns.get_loc('車有P')+1).value = '打鍵中' 
+        df.iloc[j-2,df.columns.get_loc('車有P')]='打鍵中'
     wb.save(FILE_NAME+'.xlsm')#いったん保存
 
     #打鍵中と入力したファイルをアップロード
@@ -84,7 +84,7 @@ while len(calc_row) > 0:
 
     if error_count == BAT_SIZE:
         print('1時間停止中')
-        sleep(1)#すべてE、エラーだったら、たぶんHPメンテ中と判断して、1時間停止
+        sleep(3600)#すべてE、エラーだったら、たぶんHPメンテ中と判断して、1時間停止
 
     ########並列で実行するため、あらためて現時点の最新版のファイルを読み出して結果を追加
     try:#ダウンロードする際、競合してUnlink *** try?y/n みたいなメッセージで止まるので、いったん削除して回避。
